eb863be4ca47b888618314659946fe6e70bcf463
Jonathan Lelievre <jo.lelievre@gmail.com>
2018-12-11 18:10:40 +0100

    update version

--- ps_linklist.php
+++ ps_linklist.php
@@ -66,7 +66,7 @@ class Ps_Linklist extends Module implements WidgetInterface
     {
         $this->name = 'ps_linklist';
         $this->author = 'PrestaShop';
-        $this->version = '3.0.1';
+        $this->version = '3.0.2';
         $this->need_instance = 0;
         $this->tab = 'front_office_features';
         $this->tabs = [
