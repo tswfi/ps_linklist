c2cf019e299abe0ff3a10cc987f68d7db91e0352
Thomas Nabord <thomas.nabord@prestashop.com>
2019-06-11 16:23:01 +0100

    Move migration content in a dedicated class

--- ps_linklist.php
+++ ps_linklist.php
@@ -26,6 +26,7 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 }
 
 use PrestaShop\PrestaShop\Core\Module\WidgetInterface;
+use PrestaShop\Module\LinkList\DataMigration;
 use PrestaShop\Module\LinkList\LegacyLinkBlockRepository;
 use PrestaShop\Module\LinkList\Presenter\LinkBlockPresenter;
 use PrestaShop\Module\LinkList\Model\LinkBlockLang;
@@ -115,7 +116,7 @@ class Ps_Linklist extends Module implements WidgetInterface
 
         if ($installed) {
             if ($this->uninstallPrestaShop16Module()) {
-                $this->migrateData();
+                (new DataMigration(Db::getInstance()))->migrateData();
             } else {
                 $installed &= $this->installFixtures();
             }
@@ -203,140 +204,6 @@ class Ps_Linklist extends Module implements WidgetInterface
             $parentUninstallClosure();
         }
         return true;
-    }
-
-    /**
-     * Retrieve content from 1.6 module, then cleanup
-     */
-    private function migrateData()
-    {
-        $db = Db::getInstance();
-        // Copy first table
-        $db->execute("INSERT INTO `" . _DB_PREFIX_ . "link_block`
-            (`id_link_block`, `id_hook`, `position`)
-            SELECT `id_cms_block`, `location`, `position`
-            FROM `" . _DB_PREFIX_ . "cms_block`"
-        );
-        // Update hook IDs (Got from BlockCMSModel in 1.6 module)
-        $relationBetweenOldLocationsAndHooks = [
-            0 => 'displayLeftColumn', // LEFT_COLUMN
-            1 => 'displayRightColumn', // RIGHT_COLUMN
-            2 => 'displayFooter', // FOOTER
-        ];
-        foreach ($relationBetweenOldLocationsAndHooks as $oldLocation => $newHookLocation) {
-            // Retrieve the cms page IDs linked in the old module
-            $content = $this->generateJsonForBlockContent([
-                'cms' => $this->getCmsIdsFromBlock($oldLocation)
-            ]);
-
-            $db->execute("UPDATE `" . _DB_PREFIX_ . "link_block`
-                SET `id_hook` = " . (int) Hook::getIdByName($newHookLocation) . ",
-                `content` = '" . pSQL($content) . "'
-                WHERE `id_hook` = " . $oldLocation
-            );
-        }
-        // Copy second table (lang)
-        $db->execute("INSERT INTO `" . _DB_PREFIX_ . "link_block_lang`
-            (`id_link_block`, `id_lang`, `name`)
-            SELECT `id_cms_block`, `id_lang`, `name`
-            FROM `" . _DB_PREFIX_ . "cms_block_lang`"
-        );
-        // Copy third table (shop)
-        $db->execute("INSERT INTO `" . _DB_PREFIX_ . "link_block_shop`
-            (`id_link_block`, `id_shop`)
-            SELECT `id_cms_block`, `id_shop`
-            FROM `" . _DB_PREFIX_ . "cms_block_shop`"
-        );
-
-        $this->migrateBlockFooter();
-
-        // Drop old tables
-        $db->execute('DROP TABLE `'._DB_PREFIX_.'cms_block`,
-            `'._DB_PREFIX_.'cms_block_lang`,
-            `'._DB_PREFIX_.'cms_block_page`,
-            `'._DB_PREFIX_.'cms_block_shop`'
-        );
-    }
-
-    private function migrateBlockFooter()
-    {
-        if (!Configuration::get('FOOTER_BLOCK_ACTIVATION')) {
-            return;
-        }
-        $db = Db::getInstance();
-
-        $linkBlock = new LinkBlock();
-        $data = [];
-        foreach (explode('|', Configuration::get('FOOTER_CMS')) as $val) {
-            list(, $cmsId) = $val;
-            $data['product'][] = $cmsId;
-        }
-        if (Configuration::get('FOOTER_PRICE-DROP')) {
-            $data['product'][] = 'prices-drop';
-        }
-        if (Configuration::get('FOOTER_NEW-PRODUCTS')) {
-            $data['product'][] = 'new-products';
-        }
-        if (Configuration::get('FOOTER_BEST-SALES')) {
-            $data['product'][] = 'best-sales';
-        }
-        if (Configuration::get('FOOTER_CONTACT')) {
-            $data['static'][] = 'contact';
-        }
-        if (Configuration::get('FOOTER_SITEMAP')) {
-            $data['static'][] = 'sitemap';
-        }
-        if (Configuration::get('PS_STORES_DISPLAY_FOOTER')) {
-            $data['static'][] = 'stores';
-        }
-        $linkBlock->content = $this->generateJsonForBlockContent($data);
-        $linkBlock->id_hook = (int) Hook::getIdByName('displayFooter');
-
-        $languages = Language::getLanguages(false);
-        foreach ($languages as $lang) {
-            $linkBlock->name[$lang['id_lang']] = 'Footer content (Migrated)';
-            $linkBlock->custom_content[$lang['id_lang']] = json_encode([
-                'title' => Configuration::get('FOOTER_CMS_TEXT_'.$lang['id_lang']),
-                'url' => '#',
-            ]);
-        }
-    }
-
-    /**
-     * Generate a JSON for the column `content` of link_block
-     * 
-     * @param array $data
-     * 
-     * @return string 
-     */
-    private function generateJsonForBlockContent(array $data)
-    {
-        return json_encode([
-            'cms' => empty($data['cms']) ? [false] : $data['cms'],
-            'static' => empty($data['static']) ? [false] : $data['static'],
-            'product' => empty($data['product']) ? [false] : $data['product'],
-        ]);
-    }
-
-    /**
-     * Get list of cms IDs from database for a given old cms_block_page
-     * 
-     * @param int $oldLocation
-     * 
-     * @return array 
-     */
-    private function getCmsIdsFromBlock($oldLocation)
-    {
-        $request = $db->executeS("SELECT id_cms FROM  `" . _DB_PREFIX_ . "cms_block_page`
-            WHERE id_cms_block = " . (int) $oldLocation . "
-            AND is_category = 0"
-        );
-
-        $ids = [];
-        foreach ($request as $row) {
-            $ids[] = $row['id_cms'];
-        }
-        return $ids;
     }
 
     public function hookActionUpdateLangAfter($params)
