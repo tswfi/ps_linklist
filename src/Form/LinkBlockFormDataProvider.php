ff0834239c22271bace9c67837b479463b1535b9
Krystian Podemski <podemski.krystian@gmail.com>
2021-01-12 23:34:39 +0100

    wrong indentation

--- src/Form/LinkBlockFormDataProvider.php
+++ src/Form/LinkBlockFormDataProvider.php
@@ -159,7 +159,7 @@ class LinkBlockFormDataProvider implements FormDataProviderInterface
                 }
 
                 foreach ($customLanguages as $idLang => $custom) {
-                        $linkBlock['custom'][$key][$idLang] = [
+                    $linkBlock['custom'][$key][$idLang] = [
                         'title' => !empty($custom['title']) ? $custom['title'] : $customLanguages[$defaultLanguageId]['title'],
                         'url' => !empty($custom['url']) ? $custom['url'] : $customLanguages[$defaultLanguageId]['url'],
                     ];
