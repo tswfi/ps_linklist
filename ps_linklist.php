b8b6a5d2034ba08a5b73f531af93b39780648392
Ibrahima SOW <ibrahima.sow@prestashop.com>
2021-06-28 08:29:18 +0000

    Bump version 5.0.3

--- ps_linklist.php
+++ ps_linklist.php
@@ -71,7 +71,7 @@ class Ps_Linklist extends Module implements WidgetInterface
     {
         $this->name = 'ps_linklist';
         $this->author = 'PrestaShop';
-        $this->version = '5.0.2';
+        $this->version = '5.0.3';
         $this->need_instance = 0;
         $this->tab = 'front_office_features';
 
